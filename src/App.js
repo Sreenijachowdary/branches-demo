b052bff85545d21e51c52e887ed63865db51d557
Sreenijachowdary <sreenijachowdary42@gmail.com>
2024-07-26 15:38:44 +0530

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-
+import Attendance from "./Attendance";
+import Classes from "./Classes";
 
 function App() {
   return (
     <>
     <h1>KodNest</h1>
+    <Classes/>
+    <Attendance/>
     </>
   );
 }
